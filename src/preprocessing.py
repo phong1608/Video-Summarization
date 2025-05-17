b76decf0ac5919affd52b17f9ee0be539002603c
phong1608 <hoangvanphong579@gmail.com>
2025-05-17 21:07:53 +0700

    minor change

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,11 @@
 from wtpsplit import SaT
+from dotenv import load_dotenv
 
+from google import genai
+load_dotenv()
+import os
+
+value = os.getenv('MY_ENV_VAR')
 
 
 class Preprocessor:
@@ -8,8 +14,17 @@ class Preprocessor:
     
     def tokenize(self,paragraph):
         sat = SaT("sat-3l")
-       
-        sat.half().to("cuda")
+        value = os.getenv('API_KEY')
 
-        sentences=sat.split(paragraph)
+        client = genai.Client(api_key=value)
+        
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", contents= f"""Hãy sửa lại đoạn văn để đúng chính tả, ngữ pháp, dấu câu và dùng từ đúng và từ nội dung của văn bản đó hãy viết lại và dài hơn tập trung vào lý thuyết chính của văn bản đó.Chỉ trả về văn bản đã được tạo và không xuống dòng  
+
+                                                    Văn bản: 
+                                                    {paragraph}
+                                                    """
+        )
+        print(response.text)
+        sentences=sat.split(response.text)
         return sentences
